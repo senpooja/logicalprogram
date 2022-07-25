52be7733b9fd0a71b15e9c0c326b4103266340f9
senpooja <poojasen744055@gmail.com>
2022-07-25 10:45:34 +0530

    TempratureConversion

---
+++ tempratureConversion/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+class main
+{
+    static void Main()
+    {
+        float Fahrenheit, Celsius;
+        Fahrenheit = 95;
+        Celsius = ((Fahrenheit - 32) * 5) / 9;
+        Console.WriteLine("Temperature in celsius is: " + Celsius);
+
+        Celsius = 45;
+        Fahrenheit = (Celsius * 9 / 5) + 32;
+        Console.WriteLine("Tempreture in Fahrenheit is : " + Fahrenheit);
+
+    }
+    
+
+}
