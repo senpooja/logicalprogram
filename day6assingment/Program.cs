1b94fb6d1e09e9b6e156af6f73caf08a01b80938
senpooja <poojasen744055@gmail.com>
2022-07-25 10:16:22 +0530

    wellcome logical program files

---
+++ day6assingment/Program.cs
@@ -0,0 +1,15 @@
+﻿int first = -1;
+int secound = 1;
+int Value =0;
+
+for (int i = 1; i < 8; i++)
+{
+
+    Value = first + secound;
+    first = secound;
+    secound = Value;
+    Console.WriteLine(Value);
+
+}
+//Console.WriteLine(first);
+//Console.WriteLine(secound);
