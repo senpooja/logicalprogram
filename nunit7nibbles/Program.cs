71262c67496eb1c59b75371a807e10a94c5fbd03
senpooja <poojasen744055@gmail.com>
2022-07-25 10:53:57 +0530

    change int into nibbles

---
+++ nunit7nibbles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class GFG
+{
+
+    // Function for swapping   
+    static int swapNibbles(int x)
+    {
+        return ((x & 0x0F) << 4 |
+                (x & 0xF0) >> 4);
+    }
+
+    // Driver code
+    public static void Main()
+    {
+        int x = 100;
+        Console.Write(swapNibbles(x));
+    }
+}
