583650518928b9df2dce37821a264dc895eb3e14
senpooja <poojasen744055@gmail.com>
2022-07-25 10:36:43 +0530

    Stop_Watch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics; // to use Stopwatch feature
+using System.Threading; // to perform multiple operations
+class Demo_Stopwatch // Demo class for implementing the Stopwatch functionality
+{
+    static void Main()
+    {
+        Stopwatch stopw = new Stopwatch();
+        stopw.Start();
+        for (int i = 0; i < 100; i++)
+        {
+            Thread.Sleep(2);
+        }
+        stopw.Stop();
+        Console.WriteLine(" Time elapsed: {0} ", stopw.Elapsed);
+    }
+}
