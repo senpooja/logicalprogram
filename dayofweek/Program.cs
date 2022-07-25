9dccb29464c9e33821c43873e3d13e48527128d2
senpooja <poojasen744055@gmail.com>
2022-07-25 10:43:36 +0530

    DayOfWeek

---
+++ dayofweek/Program.cs
@@ -0,0 +1,24 @@
+﻿// C# program to find day of a given date
+using System;
+
+class GFG
+{
+
+    static int dayofweek(int d, int m, int y)
+    {
+        int[] t = { 0, 3, 2, 5, 0, 3, 5,
+                            1, 4, 6, 2, 4 };
+        y -= (m < 3) ? 1 : 0;
+
+        return (y + y / 4 - y / 100 + y / 400
+                         + t[m - 1] + d) % 7;
+    }
+
+    // Driver Program to test above function
+    public static void Main()
+    {
+        int day = dayofweek(3, 8, 2010);
+
+        Console.Write(day);
+    }
+}
